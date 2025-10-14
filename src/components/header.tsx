d4df7d8b69169cb09f790c914c4d01a4483afc24
mebur.dev <mebur.dev@gmail.com>
2025-10-14 00:08:16 -0400

    Header and Translations Fixes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,20 @@
-import React from "react";
-import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
+
 import { useTranslation } from "next-i18next";
 
 const HeaderComponent: React.FC = () => {
   const { t } = useTranslation("common");
+  const [showAbout, setShowAbout] = useState(false);
+
+  const toggleShowAbout = () => {
+    setShowAbout(!showAbout);
+  };
 
   return (
     <header id="header">
       <div className="flex justify-between text-white min-h-60 w-full pb-5 md:pb-5 px-4 shadow-md h-full">
-        <div className="text-2xl flex-col">
+        <div className="text-2xl flex-col w-3/12">
           <div
             className="font-extrabold"
             style={{ fontFamily: "var(--font-philosopher)" }}
@@ -22,13 +28,39 @@ const HeaderComponent: React.FC = () => {
             {t("developer")}
           </div>
         </div>
-        <div className="text-2xl font-bold">
-          <Link
-            href="/#experience"
-            className="hover:text-zinc-500 transition-colors duration-300 ease-out focus:outline-none focus:ring-2 focus:ring-white"
-          >
-            About
-          </Link>
+        <div className="text-2xl font-bold flex w-9/12">
+          <div className="flex justify-between items-start w-full h-full">
+            <div className="w-5/6 flex-shrink-0 m-auto">
+              {showAbout && (
+                <div className="flex items-center justify-start">
+                  <div className="flex-shrink-0 pl-12">
+                    <Image
+                      src="/images/personal-picture.jpg"
+                      alt="Levi Josue Medina Burguillos - Perfil"
+                      className="w-36 h-36 rounded-full mr-6 shadow-xl border-2 border-zinc-700"
+                      width={144}
+                      height={144}
+                    />
+                  </div>
+                  <div className="flex-grow">
+                    <div
+                      className="h-full px-10 pt-5 text-base"
+                      style={{ fontFamily: "var(--font-philosopher)" }}
+                    >
+                      {t("about-me-text")}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div
+              className="hover:text-zinc-500 transition-colors duration-300 ease-out focus:outline-none focus:ring-2 focus:ring-white self-start"
+              onClick={toggleShowAbout}
+            >
+              {t("about")}
+            </div>
+          </div>
         </div>
       </div>
     </header>
