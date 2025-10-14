0558d08c7513a3750c0ac9ea111e6531eeed7c7f
mebur.dev <mebur.dev@gmail.com>
2025-10-14 01:01:53 -0400

    LAST

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,12 @@
 // next-i18next.config.js
 
 // Usamos require en lugar de import para mantener la sintaxis CommonJS
+const path = require("path");
 
 module.exports = {
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
+    localePath: path.resolve("./public/static/locales"),
   },
 };
